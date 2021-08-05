a4a9b6a6c95a8bc628d33ea7c26dc07097cba0bd
Emily Tsen <77694772+emilytsen@users.noreply.github.com>
2021-08-05 10:53:50 -0300

    add task button

--- taskList.js
+++ taskList.js
@@ -1,27 +1,58 @@
-const createTask = (event) => {
-
-    event.preventDefault();
-    //"event" é um objeto que possui um método chamado "preventDefault()" quer vai ser executado e vai evitar com que a página recaregue ao clicar no botão;
-
-    const listTask = document.querySelector('[data-list]');
-    const inputTask = document.querySelector('[data-form-input]');
-    //conectar e selecionar o input;
-    const valueInputTask = inputTask.value;
-    //pegar valor de inputTask;
-
-    const task = document.createElement('li');
-    task.classList.add('task');
-    const contentTask = `<p class="content">${valueInputTask}</p>`;
-
-    task.innerHTML = contentTask;
-
-    listTask.appendChild(task);
-    inputTask.value = " ";
-    //Pra ficar vazio o input após o click;
-
-}
+// “IIFE”, ou Immediately Invoked Function Expression ou “Função de Invocação Imediata”
+// Serve para deixar o código mais seguro.
 
 
-const btnTask = document.querySelector('[data-form-button]');
 
-btnTask.addEventListener('click', createTask);
+(() => {
+    const createTask = (event) => {
+
+        event.preventDefault();
+        //"event" é um objeto que possui um método chamado "preventDefault()" quer vai ser executado e vai evitar com que a página recaregue ao clicar no botão;
+
+        const listTask = document.querySelector('[data-list]');
+        const inputTask = document.querySelector('[data-form-input]');
+        //conectar e selecionar o input;
+        const valueInputTask = inputTask.value;
+        //pegar valor de inputTask;
+
+        const task = document.createElement('li');
+        task.classList.add('task');
+        //adicionar a class para a estilização;
+        const contentTask = `<p data-task>${valueInputTask}</p>`;
+        task.innerHTML = contentTask;
+
+        task.appendChild(check());
+
+        listTask.appendChild(task);
+
+        //coloca também coloca um item abaixo do outro;
+        inputTask.value = " ";
+        //Pra ficar vazio o input após o click;
+
+    }
+
+
+    const btnTask = document.querySelector('[data-form-button]');
+    btnTask.addEventListener('click', createTask);
+
+    //componente Botão:
+    const check = () => {
+        const btnCheck = document.createElement('button');
+        btnCheck.classList.add('btnCheck');
+
+        btnCheck.innerText = '✿';
+
+        btnCheck.addEventListener('click', finish);
+
+        return btnCheck;
+    }
+
+    const finish = (event) => {
+
+        const btnCheck = event.target;
+
+        const finishedTask = btnCheck.parentElement;
+
+        finishedTask.classList.toggle('done'); //true or false
+    }
+})()
